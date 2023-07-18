95d7eb96e27a020acefb466569773085d33890be
PetarPetrov01 <petyr_p@mail.bg>
2023-07-18 22:51:02 +0300

    Creating template.js
    A file containing the needed template

--- JS-Applications/Client side Rendering/01.Contacts/app.js
+++ JS-Applications/Client side Rendering/01.Contacts/app.js
@@ -1,21 +1,7 @@
-import { html, render } from './node_modules/lit-html/lit-html.js'
+import {  render } from './node_modules/lit-html/lit-html.js'
 import { contacts } from './contacts.js'
+import { contactTemplate} from '/template.js'
 
-const contactTemplate = (contact) => html`
-<div class="contact card">
-    <div>
-        <i class="far fa-user-circle gravatar"></i>
-    </div>
-    <div class="info">
-        <h2>Name: ${contact.name}</h2>
-        <button class="detailsBtn">Details</button>
-        <div class="details" id=${contact.id}>
-            <p>Phone number: ${contact.phoneNumber}</p>
-            <p>Email: ${contact.email}</p>
-        </div>
-    </div>
-</div>
-`
 const contactsDiv = document.querySelector('#contacts')
 render(contacts.map(contactTemplate), contactsDiv)
 
