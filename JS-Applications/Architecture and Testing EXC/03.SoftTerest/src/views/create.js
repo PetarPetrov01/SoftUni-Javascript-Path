10aadf22011e55cceffadf39beb05da69280adb8
PetarPetrov01 <petyr_p@mail.bg>
2023-07-18 22:44:58 +0300

    Added creating functionality

--- JS-Applications/Architecture and Testing EXC/03.SoftTerest/src/views/create.js
+++ JS-Applications/Architecture and Testing EXC/03.SoftTerest/src/views/create.js
@@ -10,3 +10,22 @@ export function showCreate(context) {
     context.showSection(section)
 }
 
+async function onSubmit(e) {
+    e.preventDefault()
+    const formData = new FormData(form)
+
+    const title = formData.get('title').trim()
+    const description = formData.get('description').trim()
+    const img = formData.get('imageURL').trim()
+
+    if (title == '' ||
+        description == '' ||
+        img == ''
+    ) {
+        return alert('All inputs must be filled!')
+    }
+
+    await createIdea({title, description, img})
+    form.reset()
+    ctx.goto('/catalog')
+}
