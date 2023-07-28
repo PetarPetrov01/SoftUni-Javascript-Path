eea4126477478b9970de572224ea73c633270652
PetarPetrov01 <petyr_p@mail.bg>
2023-07-28 22:16:30 +0300

    Authentication functionality
    Login register pages and their respective REST service request

--- JS-Applications/EXAMS/CosmeticKingdom_Dec_22/src/app.js
+++ JS-Applications/EXAMS/CosmeticKingdom_Dec_22/src/app.js
@@ -5,10 +5,21 @@ import { addRender } from './middlewares/middlwares.js';
 import { updateSession } from './middlewares/session.js';
 
 //All views imports
+import { catalogPage } from './views/catalog.js';
+import { createPage } from './views/create.js';
+import { buyProdcut, detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
 
 page(updateSession)
 page(addRender);
 
+page('/index.html', '/');
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
 
 //initiliaze
 page.start();
--- JS-Applications/EXAMS/CosmeticKingdom_Dec_22/src/middlewares/middlwares.js
+++ JS-Applications/EXAMS/CosmeticKingdom_Dec_22/src/middlewares/middlwares.js
@@ -1,6 +1,24 @@
 import { html, render } from "../lib.js"
 
 
+const navTemplate = (user) => html`
+<a id="logo" href="/"><img id="logo-img" src="./images/logo.png" alt="" /></a>
+<nav>
+  <div>
+    <a href="/catalog">Products</a>
+  </div>
+  ${user
+        ? html`<!-- Logged-in users -->
+    <div class="user">
+        <a href="/create">Add Product</a>
+        <a href="/logout">Logout</a>
+    </div>`
+        : html`<!-- Guest users -->
+    <div class="guest">
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+    </div>`}
+</nav>`
 
 const root = document.querySelector('#wrapper main')
 const header = document.querySelector('#wrapper header')
@@ -10,6 +28,7 @@ function ctxRender(content) {
 }
 
 export function addRender(ctx, next) {
+    render(navTemplate(ctx.user), header)
     ctx.render = ctxRender
 
     next()
