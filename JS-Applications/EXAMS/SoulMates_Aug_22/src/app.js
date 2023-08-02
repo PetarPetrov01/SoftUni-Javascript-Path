b86bf6825d5192c77688b7b239455eebb7fc790e
PetarPetrov01 <petyr_p@mail.bg>
2023-08-02 16:03:41 +0300

    Authorization pages rendering and logic
    Implement login, register and home page

--- JS-Applications/EXAMS/SoulMates_Aug_22/src/app.js
+++ JS-Applications/EXAMS/SoulMates_Aug_22/src/app.js
@@ -1,7 +1,13 @@
 import { page } from './lib.js';
 import { addRender, addSearch } from './middlewares/middlwares.js';
 import { updateSession } from './middlewares/session.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
 page(updateSession);
 page(addRender);
 page(addSearch);
+page('/index.html', '/');
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
 page.start();
