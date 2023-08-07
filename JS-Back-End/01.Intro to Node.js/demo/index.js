614800623b8121981122f5955b2b69205a6924c6
PetarPetrov01 <petyr_p@mail.bg>
2023-08-07 22:04:30 +0300

    Initialize application

---
+++ JS-Back-End/01.Intro to Node.js/demo/index.js
@@ -0,0 +1,2 @@
+const router = require('./router')
+const server = http.createServer(router.match)
---
+++ JS-Back-End/01.Intro to Node.js/demo/router.js
@@ -0,0 +1,31 @@
+const routes = {}
+
+function register(method, path, handler) {
+    if (routes[path] == undefined) {
+        routes[path] = {}
+    }
+    routes[path][method] = handler
+}
+
+function match(req, res) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const actions = routes[url.pathname]
+    let handler;
+
+    if (actions != undefined) {
+        handler = actions[req.method]
+    }
+
+    if (typeof handler == 'function') {
+        handler(req, res)
+    } else {
+        routes.default['GET'](req, res)
+    }
+}
+
+module.exports = {
+    register,
+    get: register.bind(null, 'GET'),
+    post: register.bind(null, 'POST'),
+    match
+}
