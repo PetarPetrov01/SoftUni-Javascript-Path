3e4053df207050916938e1fbbca64c138904b667
PetarPetrov01 <petyr_p@mail.bg>
2023-08-08 21:53:40 +0300

    implement all routes

--- JS-Back-End/01.Intro to Node.js/demo/index.js
+++ JS-Back-End/01.Intro to Node.js/demo/index.js
@@ -1,6 +1,14 @@
 const http = require('http')
 const router = require('./router')
+const { defaultPage, homePage } = require('./controllers/home')
+const { catalogPage, createPage, createHuman } = require('./controllers/catalog')
+
 router.get('/', homePage)
+router.get('/catalog', catalogPage)
+router.get('/create', createPage)
+router.post('/create', createHuman)
 router.get('default', defaultPage)
+
 const server = http.createServer(router.match)
 server.listen(3000)
+
