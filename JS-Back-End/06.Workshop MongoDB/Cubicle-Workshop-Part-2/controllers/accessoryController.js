287acd483e319d0ec541645ee7679a79aaac7057
PetarPetrov01 <petyr_p@mail.bg>
2023-08-23 19:29:15 +0300

    Attach accessory and finalize app

--- JS-Back-End/06.Workshop MongoDB/Cubicle-Workshop-Part-2/controllers/accessoryController.js
+++ JS-Back-End/06.Workshop MongoDB/Cubicle-Workshop-Part-2/controllers/accessoryController.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const { createAccessory, getAllAccessories, attachAccessory } = require('../services/accessoryService');
 const { getById } = require('../services/cubeServices');
+
+router.get('/attach/:cubeId', async (req, res) => {
+    const id = req.params.cubeId;
+    const cube = await getById(id);
+    const accessories = await getAllAccessories();
+    res.render('accessoryAttach', { cube, accessories });
+});
+
+
 router.get('/create', (req, res) => {
     res.render('accessoryCreate');
 });
@@ -9,4 +18,12 @@ router.post('/create', async (req, res) => {
     const accessory = req.body;
     const result = await createAccessory(accessory);
     res.redirect('/');
+});
+
+router.post('/attach/:cubeId', async (req, res) => {
+    const id = req.params.cubeId;
+    await attachAccessory(id, Object.values(req.body));
+    res.redirect(`/accessory/attach/${id}`);
+});
+
 module.exports = router;
