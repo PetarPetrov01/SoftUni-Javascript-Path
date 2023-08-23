57f14fef848b924487fa356a3df31560dcfa008d
PetarPetrov01 <petyr_p@mail.bg>
2023-08-23 19:28:37 +0300

    Accessory implementation

---
+++ JS-Back-End/06.Workshop MongoDB/Cubicle-Workshop-Part-2/controllers/accessoryController.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { createAccessory, getAllAccessories, attachAccessory } = require('../services/accessoryService');
+const { getById } = require('../services/cubeServices');
+router.get('/create', (req, res) => {
+    res.render('accessoryCreate');
+});
+
+router.post('/create', async (req, res) => {
+    const accessory = req.body;
+    const result = await createAccessory(accessory);
+    res.redirect('/');
+module.exports = router;
