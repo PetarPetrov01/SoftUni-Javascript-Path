1f5db204d116d20a8b178c931e45c26ab4144648
PetarPetrov01 <petyr_p@mail.bg>
2023-08-30 09:34:05 +0300

    Edit, search and enroll functionality (finalize app)

--- JS-Back-End/EXAMS/Tutorials-Nov-20/controllers/courseController.js
+++ JS-Back-End/EXAMS/Tutorials-Nov-20/controllers/courseController.js
@@ -1,3 +1,9 @@
+const { Course } = require('../models/course');
+const { createCourse, getById, editCourse, enrollOnCourse, searchCourse } = require('../services/courseService');
+const { errorParser } = require('../utils/parser');
+
+const courseController = require('express').Router();
+
 courseController.get('/:id/details', async (req, res) => {
     const course = await getById(req.params.id);
 
@@ -14,11 +20,21 @@ courseController.get('/:id/details', async (req, res) => {
     });
 
 });
+
 courseController.get('/create', (req, res) => {
     res.render('create');
 });
+
 courseController.get('/:id/edit', async (req, res) => {
+    const course = await getById(req.params.id);
+
+    if (course.ownerId != req.user._id) {
+        return res.redirect('/');
+    }
+
+    res.render('edit', { course });
 });
+
 courseController.post('/create', async (req, res) => {
     try {
         if (Object.values(req.body).some(v => v == '')) {
@@ -33,10 +49,67 @@ courseController.post('/create', async (req, res) => {
         });
     }
 });
+
 courseController.post('/:id/edit', async (req, res) => {
+    try {
+        if (Object.values(req.body).some(v => v == '')) {
+            throw new Error('All inputs must be filled!');
+        }
+
+        await editCourse(req.params.id, req.body);
+
+        res.redirect(`/course/${req.params.id}/details`);
+    } catch (error) {
+        const course = req.body;
+        course._id = req.params.id;
+
+        res.render('edit', {
+            error: errorParser(error),
+            course
+        });
+    }
 });
+
 courseController.get('/:id/delete', async (req, res) => {
+    await Course.findByIdAndDelete(req.params.id);
+    res.redirect('/');
 });
+
 courseController.get('/:id/enroll', async (req, res) => {
+    const course = await getById(req.params.id);
+    console.log(req.user);
+    try {
+        if (course.ownerId == req.user._id) {
+            course.isEnrolled = true;
+            throw new Error('You can\'t enroll in your own course!');
+        }
+
+        await enrollOnCourse(req.params.id, req.user._id);
+        res.redirect(`/course/${req.params.id}/details`);
+    } catch (error) {
+        res.render('details', {
+            error: errorParser(error),
+            course
+        });
+    }
 });
+
+courseController.post('/search', async (req, res) => {
+    const courses = await searchCourse(req.body.title);
+    courses
+        .sort((a, b) => b.createdAt - a.createdAt)
+        .map(c => {
+            c.createdAt = c.createdAt
+                .toString()
+                .slice(0, c.createdAt.toString().indexOf('GMT'))
+                .trim();
+            return c;
+        });
+    res.render('home-user', {
+        courses
+    });
+});
+
+
+
 module.exports = courseController;
--- JS-Back-End/EXAMS/Tutorials-Nov-20/services/courseService.js
+++ JS-Back-End/EXAMS/Tutorials-Nov-20/services/courseService.js
@@ -1,16 +1,56 @@
 const { Course } = require('../models/course');
+
 async function getAll() {
     const courses = await Course.find({}).lean();
     return courses;
 }
+
 async function getById(courseId) {
     const course = await Course.findById(courseId).lean();
     return course;
 }
+
+async function createCourse(body, ownerId) {
+    let createdAt = new Date();
+
+    const course = {
+        title: body.title,
+        description: body.description,
+        imageUrl: body.imageUrl,
+        duration: body.duration,
+        createdAt,
+        ownerId
+    };
+    console.log(course);
+    await Course.create(course);
+}
+
 async function editCourse(id, body) {
+    const course = await Course.findById(id); //DO NOT LEAN !!
+
+    course.title = body.title;
+    course.description = body.description;
+    course.imageUrl = body.imageUrl;
+    course.duration = body.duration;
+
+    await course.save();
 }
+
 async function enrollOnCourse(courseId, userId) {
+    const course = await Course.findById(courseId);
+    console.log(course);
+    if (course.users.includes(userId)) {
+        throw new Error('You can\'t enroll twice!');
+    }
+
+    course.users.push(userId);
+    await course.save();
 }
+
 async function searchCourse(title) {
+    const courses = await Course.find({ title: new RegExp(title, 'i') }).lean();
+    return courses;
 }
+
 module.exports = { getAll, createCourse, getById, editCourse, enrollOnCourse, searchCourse };
+
