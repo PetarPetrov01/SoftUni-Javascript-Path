df76e437f03cbf32a542e9b7becdef0332172e8d
PetarPetrov01 <petyr_p@mail.bg>
2023-08-31 14:55:11 +0300

    Details and edit

--- JS-Back-End/EXAMS/BookingUni_Dec-20/controllers/hotelController.js
+++ JS-Back-End/EXAMS/BookingUni_Dec-20/controllers/hotelController.js
@@ -1,3 +1,28 @@
+const { create, getById, update, deleteById, bookRoom } = require('../services/hotelService');
+const { errorParser } = require('../utils/parser');
+
+const hotelController = require('express').Router();
+
+
+hotelController.get('/create', (req, res) => {
+    res.render('create');
+});
+
+hotelController.post('/create', async (req, res) => {
+    try {
+        if (Object.values(req.body).some(v => v == '')) {
+            throw new Error('All inputs must be filled!');
+        }
+
+        await create(req.body, req.user._id);
+        res.redirect('/');
+    } catch (error) {
+        res.render('create', {
+            error: errorParser(error),
+            body: req.body
+        });
+    }
+});
 
 hotelController.get('/:id', async (req, res) => {
     const hotel = await getById(req.params.id);
