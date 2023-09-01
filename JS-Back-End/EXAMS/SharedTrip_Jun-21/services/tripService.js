b40cf0a0b92cdaf681dc03ca0c7ed74ff6571900
PetarPetrov01 <petyr_p@mail.bg>
2023-09-01 14:00:06 +0300

    Edit and detail pages

---
+++ JS-Back-End/EXAMS/SharedTrip_Jun-21/services/tripService.js
@@ -0,0 +1,53 @@
+const { Trip } = require('../models/trip');
+
+async function getAll() {
+    return await Trip.find({}).lean();
+}
+
+async function getById(id) {
+    return await Trip.findById(id).lean().populate('buddies creator');
+}
+
+async function createTrip(data, userId) {
+
+    const trip = {
+        startPoint: data.startPoint,
+        endPoint: data.endPoint,
+        date: data.date,
+        time: data.time,
+        carImage: data.carImage,
+        carBrand: data.carBrand,
+        seats: Number(data.seats),
+        price: Number(data.price),
+        description: data.description,
+        creator: userId,
+    };
+
+    await Trip.create(trip);
+}
+
+async function editTrip(id, data, userId) {
+
+    const trip = await Trip.findById(id);
+
+    if (trip.creator._id != userId) {
+        console.log('You can\'t edit someone else\'s trip');
+    }
+
+    trip.startPoint = data.startPoint;
+    trip.endPoint = data.endPoint;
+    trip.date = data.date;
+    trip.time = data.time;
+    trip.carImage = data.carImage;
+    trip.carBrand = data.carBrand;
+    trip.seats = Number(data.seats);
+    trip.price = Number(data.price);
+    trip.description = data.description;
+
+    await trip.save();
+}
+
+async function joinTrip(id, userId) {
+}
+
+module.exports = { getAll, getById, createTrip, editTrip, joinTrip, getUserTrips };
