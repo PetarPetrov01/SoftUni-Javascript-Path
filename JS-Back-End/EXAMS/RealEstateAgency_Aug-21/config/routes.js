ab9f4a42f212f357b6621a56ee7ba41dd99aa389
PetarPetrov01 <petyr_p@mail.bg>
2023-09-06 17:57:06 +0300

    Initialize project (scaffold loading)

---
+++ JS-Back-End/EXAMS/RealEstateAgency_Aug-21/config/routes.js
@@ -0,0 +1,10 @@
+const authController = require('../controllers/authController');
+const homeController = require('../controllers/homeController');
+const offerController = require('../controllers/offerController');
+
+
+module.exports = (app) => {
+    console.log('Routes loading');
+    app.use(homeController);
+};
+
