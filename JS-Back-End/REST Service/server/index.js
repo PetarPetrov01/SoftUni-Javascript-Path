fc907b4816003e08fcfe6fedf3d36ca53a220234
PetarPetrov01 <petyr_p@mail.bg>
2023-09-15 00:24:23 +0300

    Initialize server (CORS headers)

---
+++ JS-Back-End/REST Service/server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('./middlewares/cors');
+const userController = require('./controllers/userController');
+
+
+const connectionString = 'mongodb://localhost:27017/furniture';
+start();
+
+async function start() {
+    await mongoose.connect(connectionString);
+    console.log('Connected to database');
+
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.listen(3030, () => console.log('Server started on 3030'));
+}
+
