19cf8fb5ff9c4b03958b349bdc72a8b78bea009a
PetarPetrov01 <petyr_p@mail.bg>
2023-09-16 13:45:24 +0300

    Turn static html template into static react app

---
+++ React-JS/01.Intro to React and JSX/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <div className='hero_area'>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
