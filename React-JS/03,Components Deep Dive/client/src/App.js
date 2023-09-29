8758fb9072f45c211d062d84e9f48a8652985164
PetarPetrov01 <petyr_p@mail.bg>
2023-09-29 21:14:13 +0300

    Fetch data and Loader

--- React-JS/03,Components Deep Dive/client/src/App.js
+++ React-JS/03,Components Deep Dive/client/src/App.js
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 function App() {
+    const [todos, setTodos] = useState([])
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const res = await fetch('http://localhost:3030/jsonstore/todos')
+                const data = await res.json();
+                setTodos(Object.values(data));
+                setLoading(false)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getData()
+    }, [])
     return (
         <div className="App">
 
@@ -12,6 +29,11 @@ function App() {
 
                     <div className="add-btn-container">
                         <button className="btn" onClick={addTodo}>+ Add new Todo</button>
+                    </div>
+                    <div className="table-wrapper">
+                        {isLoading
+                            ? <Loader />
+                            : <TodoList todos={todos} toggleCompletion={toggleCompletion} />}
                     </div>
                 </section>
             </main>
