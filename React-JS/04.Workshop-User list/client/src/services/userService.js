17c018bb6c07fd2c8fb99a59ffa455dee5abdb97
PetarPetrov01 <petyr_p@mail.bg>
2023-09-30 17:05:34 +0300

    userService implementation

---
+++ React-JS/04.Workshop-User list/client/src/services/userService.js
@@ -0,0 +1,51 @@
+const host = 'http://localhost:3005/api/';
+
+async function api(method, url, body) {
+    const options = {
+        method,
+        headers: {}
+    };
+
+    if (body) {
+        options.headers["Content-Type"] = "application/json";
+        options.body = JSON.stringify(body);
+    }
+
+    try {
+        const res = await fetch(`${host}${url}`, options);
+        const data = res.json();
+
+        return data;
+    } catch (error) {
+        alert(error);
+        throw (error);
+    }
+}
+
+const get = api.bind(null, 'GET');
+const post = api.bind(null, 'POST');
+const put = api.bind(null, 'PUT');
+const del = api.bind(null, 'DELETE');
+
+async function getAll(query) {
+    return await get('users');
+}
+
+async function getById(userId) {
+    return await get(`users/${userId}`);
+}
+
+async function create(data) {
+    return post('users', data);
+}
+
+async function update(userId, data) {
+    return await put(`users/${userId}`, data);
+}
+
+async function deleteById(userId) {
+    return await del(`users/${userId}`);
+}
+
+export { getAll, getById, create, update, deleteById };
+
