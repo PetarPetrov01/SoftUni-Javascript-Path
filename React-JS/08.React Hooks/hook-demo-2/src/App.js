9399a2de9c7366b2464ea416bd2fcccdac9e5555
PetarPetrov01 <petyr_p@mail.bg>
2023-10-14 21:51:01 +0300

    Manage the state with useReducer

--- React-JS/08.React Hooks/hook-demo-2/src/App.js
+++ React-JS/08.React Hooks/hook-demo-2/src/App.js
@@ -1,3 +1,21 @@
+import './App.css'
+
+import { useEffect, useReducer } from "react";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add':
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
+function App() {
+
+  const [people, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+
+  }, [])
   return (
     <div className="App">
       <div style={{ margin: "auto",padding: "15px",border:"1px groove" }}>
