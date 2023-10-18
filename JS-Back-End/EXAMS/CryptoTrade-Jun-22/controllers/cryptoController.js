d2488be450a32818f9bdf56ed7f9046cb57f6363
PetarPetrov01 <petyr_p@mail.bg>
2023-10-18 16:52:51 +0300

    Implement catalog

--- JS-Back-End/EXAMS/CryptoTrade-Jun-22/controllers/cryptoController.js
+++ JS-Back-End/EXAMS/CryptoTrade-Jun-22/controllers/cryptoController.js
@@ -1,6 +1,34 @@
 const { errorParser } = require('../utils/parser');
 const cryptoService = require('../services/cryptoService');
+
 const cryptoController = require('express').Router();
+
+cryptoController.get('/catalog', async (req, res) => {
+    const cryptos = await cryptoService.getAll();
+
+    res.render('catalog', {
+        title: 'All',
+        cryptos
+    });
+});
+
+cryptoController.get('/:id/details', async (req, res) => {
+
+    const crypto = await cryptoService.getById(req.params.id);
+    crypto.isOwner = req.user?._id == crypto.owner;
+
+    if (req.user) {
+        crypto.isUser = true;
+        crypto.hasBought = crypto.bought.some((id) => id == req.user._id);
+    }
+
+    res.render('details', {
+        title: `${crypto.name} details`,
+        crypto
+    });
+
+});
+
 cryptoController.post('/create', isUser(), async (req, res) => {
     try {
         if (Object.values(req.body).some(v => v == '')) {
