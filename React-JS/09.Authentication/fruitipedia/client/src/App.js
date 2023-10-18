0cabd4b65d562ef35e4b8ed85ef235fd3e908913
PetarPetrov01 <petyr_p@mail.bg>
2023-10-18 18:51:05 +0300

    Fruit catalog implementation

--- React-JS/09.Authentication/fruitipedia/client/src/App.js
+++ React-JS/09.Authentication/fruitipedia/client/src/App.js
@@ -1,4 +1,21 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import * as fruitService from './services/fruitService';
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Home } from "./components/Home/Home";
 function App() {
+    const navigate = useNavigate();
+    const [fruits, setFruits] = useState();
+
+    useEffect(() => {
+        fruitService.getAll()
+            .then(result => {
+                setFruits(result);
+            })
+            .catch(er => alert(er));
+    }, []);
+
 
     return (
         <div className="App">
@@ -9,6 +26,7 @@ function App() {
                     <main>
                         <Routes>
                             <Route path="/" element={<Home />}></Route>
+                            <Route path="/catalog" element={<Catalog fruits={fruits} />}></Route>
                         </Routes>
                     </main>
                 </div>
@@ -17,3 +35,5 @@ function App() {
         </div >
     );
 }
+
+export default App;
--- React-JS/09.Authentication/fruitipedia/client/src/services/fruitService.js
+++ React-JS/09.Authentication/fruitipedia/client/src/services/fruitService.js
@@ -4,4 +4,27 @@ const baseUrl = 'http://localhost:3030/data/fruits/';
 
 export const getAll = async (search = '') => {
     const result = await api.get(baseUrl);
+    return search
+        ? Object.values(result)
+            .filter(f => f.name.toLowerCase().includes(search.toLowerCase()))
+        : Object.values(result);
+    //jsonstore does not work with query params!!!
+};
+
+export const create = async (data) => {
+    return await api.post(baseUrl, data);
+};
+
+export const getById = async (id) => {
+    const result = await api.get(`${baseUrl}${id}`);
+    return result;
+};
+
+export const edit = async (id, body) => {
+    const result = await api.put(`${baseUrl}${id}`, body);
+    return result;
+};
+
+export const deleteById = async (id) => {
+    await api.del(`${baseUrl}${id}`);
 };
