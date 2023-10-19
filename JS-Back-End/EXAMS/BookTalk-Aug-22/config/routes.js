93872db6f3b3224ec0e3551e55451e99d3180274
PetarPetrov01 <petyr_p@mail.bg>
2023-10-19 22:28:49 +0300

    Initialize app

---
+++ JS-Back-End/EXAMS/BookTalk-Aug-22/config/routes.js
@@ -0,0 +1,8 @@
+const bookController = require('../controllers/BookController');
+const authController = require('../controllers/authController');
+const homeController = require('../controllers/homeController');
+const missingController = require('../controllers/missingController');
+
+module.exports = (app) => {
+    console.log('Routes loading');
+};
