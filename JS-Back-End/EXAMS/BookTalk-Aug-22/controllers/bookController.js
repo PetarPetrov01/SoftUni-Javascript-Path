ce3665b3d19d3dffd7290812741839cd4b0ce33e
PetarPetrov01 <petyr_p@mail.bg>
2023-10-21 16:21:19 +0300

    Catalog view

--- JS-Back-End/EXAMS/BookTalk-Aug-22/controllers/bookController.js
+++ JS-Back-End/EXAMS/BookTalk-Aug-22/controllers/bookController.js
@@ -1,4 +1,18 @@
 const { isUser, isOwner } = require('../middlewares/guards');
+const bookController = require('express').Router();
+const bookService = require('../services/bookService');
+
+bookController.get('/catalog', async (req, res) => {
+
+    const books = await bookService.getAll();
+
+    res.render('catalog', {
+        title: 'Book Catalog',
+        books
+    });
+
+});
+
 bookController.get('/create', isUser(), (req, res) => {
     res.render('create', {
         title: 'Create',
