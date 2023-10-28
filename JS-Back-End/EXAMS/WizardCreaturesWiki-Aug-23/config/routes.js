4343e912ecb2b57fbdaf6951bdf823d60c6351ab
PetarPetrov01 <petyr_p@mail.bg>
2023-10-28 15:13:57 +0300

    Initialize and configure

---
+++ JS-Back-End/EXAMS/WizardCreaturesWiki-Aug-23/config/routes.js
@@ -0,0 +1,8 @@
+const authController = require('../controllers/authController');
+const creatureController = require('../controllers/creatureController');
+const homeController = require('../controllers/homeController');
+const missingController = require('../controllers/missingController');
+
+module.exports = (app) => {
+    console.log('Routes loading');
+};
