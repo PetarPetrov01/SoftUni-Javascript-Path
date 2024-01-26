1600e39209267d11cc71ac0374175f592a8bbf10
PetarPetrov01 <petyr_p@mail.bg>
2024-01-26 17:56:42 +0200

    Extract todo-item component

---
+++ Angular/02.Intro-to-RxJS/todo-list/src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from '../type/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+export class TodoItemComponent {
+  @Input() todo: Todo = {
+    title: '',
+    completed: false
+  };
+}
--- Angular/02.Intro-to-RxJS/todo-list/src/app/todo-list/todo-list.component.ts
+++ Angular/02.Intro-to-RxJS/todo-list/src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
+import { Todo } from '../type/Todo';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor,TodoItemComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-
+  todos: Todo[] = [
+    {  title: 'Do some task',  completed: false},
+    {  title: 'Do some task even more',  completed: false},
+    {  title: 'Do something',  completed: false},
+  ]
 }
