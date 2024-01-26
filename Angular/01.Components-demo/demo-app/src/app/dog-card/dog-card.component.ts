16977501c70e1e73195ce661fe23d025fd1317da
PetarPetrov01 <petyr_p@mail.bg>
2024-01-26 12:35:23 +0200

    Implement dog cards
    Iterate the dogs array and for each dog create a component (dog-card)

--- Angular/01.Components-demo/demo-app/src/app/dog-card/dog-card.component.ts
+++ Angular/01.Components-demo/demo-app/src/app/dog-card/dog-card.component.ts
@@ -8,5 +8,19 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './dog-card.component.css'
 })
 export class DogCardComponent implements OnInit{
+  @Input('dog') dog: { name: string, age: number, imgUrl: string } = {
+    name:'',
+    age: 0,
+    imgUrl: ''
+  };
 
+  ngOnInit(): void {
+    console.log(this.dog);
+  }
+
+  showInfo = true;
+
+  handleClick(event: Event) {
+    this.showInfo = !this.showInfo;
+  }
 }
--- Angular/01.Components-demo/demo-app/src/app/dogs/dogs.component.ts
+++ Angular/01.Components-demo/demo-app/src/app/dogs/dogs.component.ts
@@ -1,15 +1,15 @@
 import { NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { DogCardComponent } from '../dog-card/dog-card.component';
 
 @Component({
   selector: 'app-dogs',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,DogCardComponent],
   templateUrl: './dogs.component.html',
   styleUrl: './dogs.component.css'
 })
 export class DogsComponent {
-
   @Input('dogArr') dogs: { name: string, age: number, imgUrl: string }[] = [];
-
+  
 }
