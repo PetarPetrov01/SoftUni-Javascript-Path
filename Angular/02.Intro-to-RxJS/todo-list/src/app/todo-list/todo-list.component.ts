b7889e492e61e8db28285bb66bf7073a374718fe
PetarPetrov01 <petyr_p@mail.bg>
2024-01-26 18:38:19 +0200

    Implement consuming of the service

--- Angular/02.Intro-to-RxJS/todo-list/src/app/todo-list/todo-list.component.ts
+++ Angular/02.Intro-to-RxJS/todo-list/src/app/todo-list/todo-list.component.ts
@@ -1,19 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
 import { Todo } from '../type/Todo';
 import { NgFor } from '@angular/common';
+import { TodoService } from '../todo.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [NgFor,TodoItemComponent],
+  imports: [NgFor, TodoItemComponent],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrl: './todo-list.component.css',
+  providers: [HttpClient]
 })
-export class TodoListComponent {
-  todos: Todo[] = [
-    {  title: 'Do some task',  completed: false},
-    {  title: 'Do some task even more',  completed: false},
-    {  title: 'Do something',  completed: false},
-  ]
+
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
+
+  constructor(private TodoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.TodoService.getTodos()
+      .subscribe((todos) => {
+        this.todos = todos;
+        console.log(todos)
+      })
+  }
+
+  createTodo(event: Event, todoInput: HTMLInputElement){
+    event.preventDefault();
+
+    const newTodo: Todo | undefined = this.TodoService.addTodo(todoInput)
+    if(newTodo != undefined){
+      this.todos.push(newTodo);
+      todoInput.value = ''
+    }
+  }
 }
