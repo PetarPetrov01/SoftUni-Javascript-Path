0fa06219552996476e95725e7f34a5c4d50f47e6
PetarPetrov01 <petyr_p@mail.bg>
2024-01-28 22:37:24 +0200

    Refactor struture and global styling

--- Angular/03.Workshop-components/client/forum/src/app/app.component.ts
+++ Angular/03.Workshop-components/client/forum/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PostsComponent } from './main/posts/posts.component';
+import { RecentsComponent } from './main/recents/recents.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    PostsComponent,
+    RecentsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
