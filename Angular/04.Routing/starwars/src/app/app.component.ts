db3ed2b348819c4382326b2506a0da2115573654
PetarPetrov01 <petyr_p@mail.bg>
2024-02-07 10:50:44 +0200

    navigation component and routes

--- Angular/04.Routing/starwars/src/app/app.component.ts
+++ Angular/04.Routing/starwars/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PeopleComponent } from './people/people.component';
 import { PlanetsComponent } from './planets/planets.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PeopleComponent, PlanetsComponent],
+  imports: [
+    RouterOutlet,
+    PeopleComponent,
+    PlanetsComponent,
+    NavigationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- Angular/04.Routing/starwars/src/app/app.routes.ts
+++ Angular/04.Routing/starwars/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PeopleComponent } from './people/people.component';
+import { PlanetsComponent } from './planets/planets.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PeopleComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'planets', component: PlanetsComponent },
+];
