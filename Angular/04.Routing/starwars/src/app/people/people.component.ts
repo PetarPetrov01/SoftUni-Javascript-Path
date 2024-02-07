b3ae2e57ca3518635d0d349a452a9a2d185d3715
PetarPetrov01 <petyr_p@mail.bg>
2024-02-07 11:38:11 +0200

    Style people component

--- Angular/04.Routing/starwars/src/app/people/people.component.ts
+++ Angular/04.Routing/starwars/src/app/people/people.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { PeopleService } from './people.service';
 import { Character } from '../types/character';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, LoaderComponent, NgIf],
   templateUrl: './people.component.html',
   styleUrl: './people.component.css',
 })
-export class PeopleComponent implements OnInit{
-peopleList: Character[] = [];
+export class PeopleComponent implements OnInit {
+  peopleList: Character[] = [];
+  isLoading: boolean = false;
 
   constructor(private peopleService: PeopleService) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
     this.peopleService.getPeople().subscribe({
-      next: (people)=>{
+      next: (people) => {
         this.peopleList = people.results.slice(0, 10);
-        console.log(this.peopleList);
+        this.isLoading = false;
       },
-      error: (err)=>console.log(err)
-    })
+      error: (err) => {
+        console.log(err);
+        this.isLoading = false;
+      },
+    });
   }
 }
