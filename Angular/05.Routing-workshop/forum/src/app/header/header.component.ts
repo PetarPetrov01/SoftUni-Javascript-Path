3708b172a31e0d532e5355c1857df7701d7861f7
PetarPetrov01 <petyr_p@mail.bg>
2024-02-08 13:27:04 +0200

    Implement login

--- Angular/05.Routing-workshop/forum/src/app/header/header.component.ts
+++ Angular/05.Routing-workshop/forum/src/app/header/header.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../user/user.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor(private userService: UserService) {}
 
+  get isLogged(){
+    return this.userService.isLogged;
+  }
+
+  get firstName(){
+    return this.userService.user?.firstName || ''
+  }
 }
--- Angular/05.Routing-workshop/forum/src/app/user/login/login.component.ts
+++ Angular/05.Routing-workshop/forum/src/app/user/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private userService: UserService) {}
 
+  login(email: string, password: string): void {
+    console.log(email, password);
+
+    this.userService.login();
+  }
 }
