d7953cb292be9f2be20a953811d9d8020540fb78
PetarPetrov01 <petyr_p@mail.bg>
2024-02-08 12:17:38 +0200

    Planets structure and styling

--- Angular/04.Routing/starwars/src/app/planets/planets.component.ts
+++ Angular/04.Routing/starwars/src/app/planets/planets.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlanetsService } from './planets.service';
+import { Planet } from '../types/planet';
+import { NgFor, NgIf } from '@angular/common';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   selector: 'app-planets',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, LoaderComponent],
   templateUrl: './planets.component.html',
-  styleUrl: './planets.component.css'
+  styleUrl: './planets.component.css',
 })
-export class PlanetsComponent {
+export class PlanetsComponent implements OnInit {
+  planets: Planet[] = [];
+  isLoading: boolean = false;
 
+  constructor(private planetsService: PlanetsService) {}
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.planetsService.getPlanets().subscribe({
+      next: (result) => {
+        this.planets = result.results.slice(0, 10);
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.log(err);
+        this.isLoading = false;
+      },
+    });
+  }
 }
