d82294ab08cd0f0f7bf4a9451c6b05a6916b7e66
PetarPetrov01 <petyr_p@mail.bg>
2024-02-09 15:58:47 +0200

    Initialize application

---
+++ Angular/06.Forms/demo/src/app/create-form/create-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-form.component.html',
+  styleUrl: './create-form.component.css'
+})
+export class CreateFormComponent {
+
+}
