843dfd060b5407bccb48f65daad7dcf4396d7ca8
PetarPetrov01 <petyr_p@mail.bg>
2024-02-09 10:35:08 +0200

    Dynamic comments

--- Angular/05.Routing-workshop/forum/src/app/main/themes/comments/comments.component.ts
+++ Angular/05.Routing-workshop/forum/src/app/main/themes/comments/comments.component.ts
@@ -1,21 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../../api.service';
+import { ThemePopulated } from '../../../types/Theme';
+import { NgFor, NgIf } from '@angular/common';
+import { UserService } from '../../../user/user.service';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './comments.component.html',
-  styleUrl: './comments.component.css'
+  styleUrl: './comments.component.css',
 })
 export class CommentsComponent implements OnInit {
+  themeId: string = '';
+  theme!: ThemePopulated;
 
+  constructor(
+    private activated: ActivatedRoute,
+    private apiService: ApiService,
+    private userService: UserService
+  ) {}
 
-  constructor(private activated: ActivatedRoute){}
+  get isLogged() {
+    return this.userService.isLogged;
+  }
 
   ngOnInit(): void {
-    this.activated.params.subscribe((params)=>{
-      console.log(params['id']);
-    })
+    this.activated.params.subscribe((params) => {
+      this.themeId = params['id'];
+    });
+
+    this.apiService.getComments(this.themeId).subscribe({
+      next: (result) => {
+        this.theme = result;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
