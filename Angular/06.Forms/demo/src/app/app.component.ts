ed200063419275e161c73e0c36f21708abae957a
PetarPetrov01 <petyr_p@mail.bg>
2024-02-10 15:35:30 +0200

    simple styling and routes

--- Angular/06.Forms/demo/src/app/app.component.ts
+++ Angular/06.Forms/demo/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CreateFormComponent } from './create-form/create-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CreateFormComponent],
+  imports: [RouterOutlet, CreateFormComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'demo';
--- Angular/06.Forms/demo/src/app/app.routes.ts
+++ Angular/06.Forms/demo/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CreateFormComponent } from './create-form/create-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'create', component: CreateFormComponent},
+    {path:'reactive', component: ReactiveFormComponent}
+];
