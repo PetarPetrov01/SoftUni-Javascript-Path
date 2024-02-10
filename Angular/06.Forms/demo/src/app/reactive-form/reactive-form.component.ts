323c3a60556518776c39f86d468aa6ee919693a2
PetarPetrov01 <petyr_p@mail.bg>
2024-02-10 15:46:11 +0200

    Reactive forms approach

--- Angular/06.Forms/demo/src/app/reactive-form/reactive-form.component.ts
+++ Angular/06.Forms/demo/src/app/reactive-form/reactive-form.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
-  styleUrl: './reactive-form.component.css'
+  styleUrl: './reactive-form.component.css',
 })
 export class ReactiveFormComponent {
+  registerForm = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  submitHandler() {
+    console.log(this.registerForm.valid);
+  }
 }
