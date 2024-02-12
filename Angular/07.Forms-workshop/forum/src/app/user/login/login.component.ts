c47ba28c31aad96de99acaf5aea21e51efc09d9c
PetarPetrov01 <petyr_p@mail.bg>
2024-02-12 22:34:20 +0200

    Implement match passwords validator

--- Angular/07.Forms-workshop/forum/src/app/user/login/login.component.ts
+++ Angular/07.Forms-workshop/forum/src/app/user/login/login.component.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
-import { Router } from '@angular/router';
-import {
-  FormBuilder,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { validateEmail } from '../../validators/email-validator';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-
 export class LoginComponent {
   loginForm = this.fb.group({
     email: ['', [Validators.required, validateEmail()]],
@@ -34,7 +29,6 @@ export class LoginComponent {
       console.log(this.loginForm.get('email')?.errors);
       return;
     }
-
 
     this.userService.login();
     this.router.navigate(['/']);
