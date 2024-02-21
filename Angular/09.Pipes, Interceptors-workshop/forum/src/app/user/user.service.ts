f1cbb9f651db668b4f83bf81fe48d6d2d78b50e6
PetarPetrov01 <petyr_p@mail.bg>
2024-02-21 11:12:01 +0200

    Handle authentication

--- Angular/09.Pipes, Interceptors-workshop/forum/src/app/user/user.service.ts
+++ Angular/09.Pipes, Interceptors-workshop/forum/src/app/user/user.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { User } from '../types/user';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private user$$ = new BehaviorSubject<User | undefined>(undefined);
+  public user$ = this.user$$.asObservable();
+
   user: User | undefined;
   USER_KEY = '[user]';
 
-  constructor() {
+  constructor(private http: HttpClient) {
     try {
       const lsUser = localStorage.getItem(this.USER_KEY) || '';
       this.user = JSON.parse(lsUser);
@@ -21,13 +26,32 @@ export class UserService {
     return !!this.user;
   }
 
-  login(): void {
-    this.user = {
-      email: 'fakeemail@mail.com',
-      firstName: 'Kiro',
-    };
+  login(email: string, password: string) {
+    return this.http
+      .post<User>('/api/login', { email, password })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
 
-    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  register(
+    username: string,
+    email: string,
+    password: string,
+    rePassword: string,
+    tel: string
+  ) {
+    return this.http
+      .post<User>('/api/register', {
+        username,
+        email,
+        password,
+        rePassword,
+        tel,
+      })
+      .pipe(
+        tap((user) => {
+          this.user$$.next(user);
+        })
+      );
   }
 
   logout(): void {
