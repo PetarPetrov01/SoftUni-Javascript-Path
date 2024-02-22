211cd84a81b33838464f158b534431b24b30a5d5
PetarPetrov01 <petyr_p@mail.bg>
2024-02-22 12:00:04 +0200

    Install NgRx and generate posts1 component

---
+++ Angular/10.State Managment/src/app/posts1/posts1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts1',
+  standalone: true,
+  imports: [],
+  templateUrl: './posts1.component.html',
+  styleUrl: './posts1.component.css'
+})
+export class Posts1Component {
+
+}
